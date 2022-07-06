b0e58aedf274f43bf99b2ad7d764b1e6b6bee965
Gabrieldlb <gabriel.delabiche@epsi.fr>
2022-07-06 20:40:00 +0200

    add directory and 1 view

---
+++ vue/view.sql
@@ -0,0 +1,17 @@
+-- Chercher les demandes qui ont été faites après une date donnée.
+-- Date de votre choix.
+
+-- Pour une demande donnée, afficher la raison sociale de l’entreprise, la tournée correspondante et la
+-- quantité à récupérer pour chaque type de déchet.
+-- Demande de votre choix.
+
+-- Afficher la quantité totale récupérée par type de déchet pour un mois/année donné.
+-- Mois et année de votre choix.
+
+-- Afficher les employés ayant réalisé moins de n tournées. Triez le résultat sur le nombre de tournées.
+-- N étant un nombre de votre choix.
+
+-- Affichez les informations de l’entreprise qui a réalisé plus de demandes que l’entreprise Formalys (ou
+-- une autre entreprise dont vous fournissez le nom).
+
+
